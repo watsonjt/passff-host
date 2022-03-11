8906c0acd0a9134078ec206cbd30b342b63d330b
Jon Watson <self@jonwatson.dev>
2022-03-11 10:36:24 -0700

    Added windows support, please read the WIN_SETUP.org file

--- src/passff.py
+++ src/passff.py
@@ -9,6 +9,7 @@ import os
 import struct
 import subprocess
 import sys
+import re
 
 VERSION = "_VERSIONHOLDER_"
 
@@ -50,6 +51,21 @@ def sendMessage(encodedMessage):
     sys.stdout.buffer.write(encodedMessage['length'])
     sys.stdout.write(encodedMessage['content'])
     sys.stdout.flush()
+
+def mk_tmp_bashrc(path_to_bashrc):
+    try:
+        with open(path_to_bashrc) as fd:
+            lines = fd.readlines()
+            no_ansi_lines = filter(
+                          lambda x: (not re.search(r"echo.+-ne",x)),
+                          lines)
+            path_to_tmprc = os.path.dirname(path_to_bashrc)+\
+                            os.path.sep+".tmp.bashrc"
+            with open(path_to_tmprc,"w") as tmpfd:
+                tmpfd.writelines(no_ansi_lines)
+            return path_to_tmprc
+    except OSError:
+        return None
 
 
 if __name__ == "__main__":
@@ -103,14 +119,50 @@ if __name__ == "__main__":
         'env': env
     }
 
-    # Run and communicate with pass script
-    proc = subprocess.run(cmd, **proc_params)
+    if "nt" in os.name:
+        path_to_posix_shell = None
+        posix_opts = None
+        where = subprocess.run(["where", "msys2"],
+                                              stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+        try:
+            path_to_posix_shell = os.path.dirname(where.stdout.decode(CHARSET).strip())\
+                                                  +r"\usr\bin\bash.exe"
+            tmprc = mk_tmp_bashrc(os.path.expanduser("~")+os.path.sep+".bashrc")
+            if tmprc:
+                tmprc = ["--rcfile", tmprc]
+            else:
+                tmprc = []
+            posix_opts = tmprc + ["-l","-c"]
+            where.check_returncode()
+        except subprocess.CalledProcessError:
+            where = subprocess.run(["where", "wsl"], stdout=subprocess.PIPE,
+                                                     stderr=subprocess.PIPE)
+            wsl_home = subprocess.run(["wsl","--","echo","$HOME"],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)\
+                             .stdout.decode(CHARSET).strip()
+            path_to_posix_shell = where.stdout.decode(CHARSET).strip()
+            posix_opts = ["--rcfile",f"{wsl_home}/.bashrc","-i","-c"]
 
+        posix_cmd = [path_to_posix_shell] + posix_opts + [" ".join(cmd)]
+        proc = subprocess.run(posix_cmd, **proc_params)
+        if proc.returncode == 0:
+            sendMessage(
+                encodeMessage({
+                    "exitCode": proc.returncode,
+                    "stdout": proc.stdout.decode(CHARSET),
+                    "stderr": b''.decode(CHARSET),
+                    "version": VERSION }))
+            sys.exit(0)
+    else:
+        proc = subprocess.run(cmd, **proc_params)
+    
     # Send response
     sendMessage(
         encodeMessage({
-            "exitCode": proc.returncode,
-            "stdout": proc.stdout.decode(CHARSET),
-            "stderr": proc.stderr.decode(CHARSET),
-            "version": VERSION
+        "exitCode": proc.returncode,
+        "stdout": proc.stdout.decode(CHARSET),
+        "stderr": proc.stderr.decode(CHARSET),
+        "version": VERSION
         }))
